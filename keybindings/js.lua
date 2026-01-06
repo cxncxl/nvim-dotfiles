b75b9f9c88bb05e00e66849b9782d30896796a08
danyil <danyil.mahalias@icloud.com>
2026-01-06 13:19:18 +0200

    feat: comment toggle for js

--- keybindings/js.lua
+++ keybindings/js.lua
@@ -26,10 +26,39 @@ local function log_variable()
     vim.api.nvim_buf_set_lines(0, end_line + 1, end_line + 1, false, { padding .. log_line })
 end
 
+local function toggle_comment()
+    local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+    vim.api.nvim_feedkeys(esc, "x", false)
+
+    vim.schedule(function()
+        local start_line = vim.fn.line("'<") - 1
+        local end_line = vim.fn.line("'>") - 1
+
+        if start_line < 0 then return end
+
+        local lines = vim.api.nvim_buf_get_lines(0, start_line, end_line + 1, false)
+        if #lines == 0 then return end
+
+        local is_commented = lines[1]:match("^%s*//")
+        local new_lines = {}
+
+        for _, line in ipairs(lines) do
+            if is_commented then
+                table.insert(new_lines, (line:gsub("^(%s*)//%s?", "%1")))
+            else
+                table.insert(new_lines, (line:gsub("^(%s*)", "%1// ")))
+            end
+        end
+
+        vim.api.nvim_buf_set_lines(0, start_line, end_line + 1, false, new_lines)
+    end)
+end
+
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
     pattern = { "*.js", "*.ts" },
     callback = function()
         vim.keymap.set("n", "<leader>l", log_variable, { silent = true, buffer = true, desc = "Log variable under cursor" })
+        vim.keymap.set("v", "<leader>/", toggle_comment, { silent = true, buffer = true, desc = "Toggle comments" })
 
         vim.keymap.set("n", "<F6>", function()
             require'dap'.run({
